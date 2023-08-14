6fb575e4535ea961fb2d1240b3a390aba8c8098a
Salih Candir <salih@live.at>
2023-08-14 20:04:25 +0200

    refactor: extract spf command to infra module (#19)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-use crate::common::presenter::Presenter;
-use crate::dns::infrastructure::dns_resolver::DomainDnsResolver;
 use clap::{Parser, Subcommand};
 
-use crate::spf::core::check::{
-    SpfSummary, SummarySpfQuery, SummarySpfTerminalPresenter, SummarySpfUseCase,
-    SummarySpfUseCaseImpl, SummarySpfWithDetailTerminalPresenter,
-};
-use crate::spf::infrastructure::cli::Spf;
+use crate::spf::infrastructure::{spf_command, Spf};
 
 pub mod common;
 pub mod dns;
@@ -30,20 +24,6 @@ async fn main() {
     let args = Cli::parse();
 
     match &args.command {
-        Commands::Spf(spf) => {
-            let mut dns_resolver_gateway = DomainDnsResolver::new();
-            let presenter: Box<dyn Presenter<SpfSummary, String>> = if spf.detail {
-                Box::new(SummarySpfWithDetailTerminalPresenter::new())
-            } else {
-                Box::new(SummarySpfTerminalPresenter::new())
-            };
-            let mut summary_spf_use_case = SummarySpfUseCaseImpl::new(&mut dns_resolver_gateway);
-
-            let query = SummarySpfQuery {
-                domain_name: spf.domain.to_owned(),
-                record: spf.record.to_owned(),
-            };
-            summary_spf_use_case.execute(&query, presenter);
-        }
+        Commands::Spf(spf) => spf_command(spf),
     }
 }
